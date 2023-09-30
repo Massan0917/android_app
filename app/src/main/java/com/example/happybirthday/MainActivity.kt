01220fbc9f7998674e1d9e32e1823a226df4bf53
Massan0917 <massan000917@gmail.com>
2023-09-30 10:39:38 +0900

    mas/dev/add background image

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,17 +4,23 @@ import android.os.Bundle
 import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -33,11 +39,10 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
-    Column( verticalArrangement = Arrangement.Center
-            //modifier = modifier.padding(8.dp)
-            )
-    {
+fun GreetingText(message : String, modifier: Modifier = Modifier) {
+    Column( verticalArrangement = Arrangement.Center,
+            modifier = modifier.padding(16.dp)
+    ){
         Text(
             text = message,
             fontSize = 100.sp,
@@ -50,13 +55,29 @@ fun GreetingText(message: String, modifier: Modifier = Modifier) {
             lineHeight = 30.sp
         )
     }
+}
 
+@Composable
+fun GreetingImage(message : String, from : String, modifier : Modifier = Modifier){
+    val image = painterResource(R.drawable.androidparty)
+    Box{
+        GreetingText(   message = "Android\nkun",
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(4.dp)
+        )
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText("Android\nkun")
+        GreetingImage(message = "Android\nkun", from ="hogehoge")
+        //GreetingText("Android\nkun")
     }
 }
